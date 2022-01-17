0916dbab13f9bef47438246dc94b0c887c3fae1e
Gourav.jain <gourav.b.jain@accenture.com>
2022-01-17 19:18:22 +0530

    addin

--- src/test/java/CucumberwithTestNGRunner/TestngRunner.java
+++ src/test/java/CucumberwithTestNGRunner/TestngRunner.java
@@ -3,6 +3,8 @@ package CucumberwithTestNGRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;	
+import org.testng.annotations.BeforeClass;
+
 
 
 @CucumberOptions(features = "src/test/java/CucumberwithTestNGRunner/BlogTest.feature", glue = {"CucumberwithTestNGRunner"})
